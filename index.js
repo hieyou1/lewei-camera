0127976e5dd241868acfff8254cf5979a8498f7e
hieyou1 <>
2021-07-09 11:15:25 -0500

    Update name to lewei-camera

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const { Drone: Controller } = require("dronelib");
-const Camera = require("./lib/capture-camera");
-const wifiConnect = require("./lib/connect-wifi");
-class Drone {
+const spawnCommand = require("spawn-command");
+const P2J = require("pipe2jpeg");
+const stream = require("stream");
+const { EventEmitter } = require("events");
+class Camera extends EventEmitter {
     /**
-     * @constructor 
+     * @function stopCam
+     * @description Stops camera stream
+     */
+    stopCam() {
+        if (!this.pybridge.killed) this.pybridge.kill("SIGINT");
+        this.pybridge = null;
+        this.stream = null;
+        this.p2j = null;
+    }
+    /**
+     * @function startStream
+     * @description Start and get camera stream
+     * @returns {stream.Readable} The H.264 stream
+     */
+    startStream() {
+        if (this.running) this.stopCam();
+        this.pybridge = spawnCommand(`pylwdrone -q stream start${((this.lowdef) ? " --low-def " : " ")}--out-file -`);
+        this.running = true;
+        this.pybridge.stderr.on("data", (a) => { console.log(a.toString()) });
+        this.pybridge.on("disconnect", console.log);
+        this.pybridge.on("error", console.error);
+        this.pybridge.on("exit", console.log);
+        this.pybridge.on("close", console.log);
+        this.stream = this.pybridge.stdout;
+        this.p2j = new P2J();
+        this.stream.pipe(this.p2j);
+        this.pybridge.on("exit", () => {
+            this.stopCam();
+            this.emit("disconnected");
+        });
+        this.p2j.on("jpeg", (jpeg) => {
+            this.jpeg = jpeg;
+            this.emit("jpeg", jpeg);
+        });
+    }
+    /**
+     * @constructor
      * @class
      * @classdesc
-     * @param {Object} opts - Options
-     * @param {Object} opts.wifi - Wi-Fi options
-     * @param {String} opts.wifi.ssid - SSID of drone's open Wi-Fi network.
-     * @param {String} [opts.wifi.password] - Password [if Wi-Fi is password-protected]
-     * @param {Boolean} opts.lowdef - Whether or not to use low definition camera stream
-     * @returns {Drone}
+     * @param {Boolean} lowdef - Whether or not to have a low definition camera (low fps and bps).
+     * @returns {Camera}
      */
-    constructor(opts) {
-        this._inited = false;
-        this.opts = opts;
-    }
-    /**
-     * @async
-     * @function Drone~init
-     * @augments Drone
-     * @returns {Promise}
-     */
-    async init() {
-        if (this._inited) return;
-        this._inited = true;
-        if (this.opts.wifi != false) await wifiConnect(this.opts.wifi.ssid, this.opts.wifi.password);
-        this.control = new Controller();
-        if (this.opts?.autoenable) this.control.enable();
-        /**
-         * @type {ReadableStream} Camera H.264 stream
-         */
-        this.camera = (new Camera(this.opts.lowdef)).getStream();
+    constructor(lowdef) {
+        super({
+            "captureRejections": true
+        });
+        this.lowdef = lowdef ?? false;
+        this.running = false;
+        this.width = 1280;
+        this.height = 720;
+        this.startStream();
     }
 }
-module.exports = Drone;
+module.exports = { Camera };
--- showcam/index.js
+++ showcam/index.js
@@ -1,21 +1,13 @@
-(async () => {
-    const Drone = require("..");
-    const drone = new Drone({
-        "wifi": false,
-        "lowdef": false,
-        "autoenable": false
-    });
-    await drone.init();
+const { Camera } = require('..');
 
-    const P2J = require("pipe2jpeg");
-    let p2j = new P2J();
+(async () => {
+    let camera = new Camera();
 
     const Express = require('express');
     const app = Express();
     app.use(Express.static("./"));
-    drone.camera.pipe(p2j);
     app.get("/image.jpg", (req, res) => {
-        res.status(200).type("image/jpg").send(p2j.jpeg);
+        res.status(200).type("image/jpg").send(camera.jpeg);
     })
     app.listen(3005);
 })();
